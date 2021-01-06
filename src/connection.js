82c8092158d4668d31acf58db0480b5ff64da66e
luca <beccibrian@gmail.com>
2021-01-06 18:54:08 -0300

    fix(all): mbps to mbs fixed

--- src/connection.js
+++ src/connection.js
@@ -13,14 +13,14 @@ async function getNetworkUploadSpeed() {
     };
     const fileSizeInBytes = 2000000
     const speed = await testNetworkSpeed.checkUploadSpeed(options, fileSizeInBytes);
-    return speed.bps / 10000000;
+    return speed.mbps / 8;
 }
 
 async function getNetworkDownloadSpeed() {
     const baseUrl = 'http://eu.httpbin.org/stream-bytes/50000000';
     const fileSizeInBytes = 50000000;
     const speed = await testNetworkSpeed.checkDownloadSpeed(baseUrl, fileSizeInBytes);
-    return speed.bps / 10000000;
+    return speed.mbps / 8;
 }
 
 module.exports = {
